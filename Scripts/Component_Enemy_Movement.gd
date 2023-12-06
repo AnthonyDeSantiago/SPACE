7bcae4ac2c6af1d047b4b6841054f8bc558318c8
Anthony <anthonyde16@gmail.com>
2023-12-05 23:40:25 -0500

    Add lil guy

--- Scripts/Component_Enemy_Movement.gd
+++ Scripts/Component_Enemy_Movement.gd
@@ -1,10 +1,13 @@
 extends CharacterBody2D
 
 @export var speed = 400
+@export var health = 100
 
-var target = position
+@onready var target = position
+@onready var anim = get_node("AnimatedSprite2D")
 
 func _physics_process(delta):
+	anim.play("run")
 	target = get_global_mouse_position()
 	velocity = position.direction_to(target) * speed
 	# look_at(target)
