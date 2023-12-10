a492098e51f98f03d49d36799e48d36ea2e77114
AnthonyDeSantiago <94334680+AnthonyDeSantiago@users.noreply.github.com>
2023-12-09 21:21:58 -0500

    Add tilemap and test player

--- Scripts/Component_Enemy_Movement.gd
+++ Scripts/Component_Enemy_Movement.gd
@@ -2,13 +2,14 @@ extends CharacterBody2D
 
 @export var speed = 400
 @export var health = 100
+@export var character_target: CharacterBody2D
 
 @onready var target = position
 @onready var anim = get_node("Marker2D/AnimatedSprite2D")
 
 func _physics_process(delta):
 	anim.play("run")
-	target = get_global_mouse_position()
+	target = character_target.position
 	velocity = position.direction_to(target) * speed
 	# look_at(target)
 	if position.distance_to(target) > 10:
