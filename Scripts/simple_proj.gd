1c12c8a5fb53c5152ecba6ac27608e14f0f47bbc
Hurkaleez <matthew.morgan105@gmail.com>
2023-12-15 15:47:04 -0500

    crossair

--- Scripts/simple_proj.gd
+++ Scripts/simple_proj.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 class_name Simple_Projectile
-@export var SPEED = 600.0
+@export var SPEED = 1200.0
 @export var damage = 25
 
 @onready var direction = (get_global_mouse_position() - global_position).normalized()
