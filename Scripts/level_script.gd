75cccbddf4dde24816fb78d8eb265a6a35b0a263
AnthonyDeSantiago <94334680+AnthonyDeSantiago@users.noreply.github.com>
2023-12-16 02:14:05 -0500

    Added Game Over Menu

--- Scripts/level_script.gd
+++ Scripts/level_script.gd
@@ -5,6 +5,7 @@ extends Node2D
 @onready var score_number = 0
 @onready var score_text_number: Label = $CanvasLayer/Score/Score_Number
 @onready var health_bar: TextureProgressBar = $"CanvasLayer/HUD/Health Bar"
+@onready var game_over_screen: CanvasLayer = $Game_Over_Screen
 
 func _on_child_entered_tree(node):
 	if node is enemy:
@@ -19,3 +20,15 @@ func _on_child_exiting_tree(node):
 
 func _on_simple_char_player_was_hurt(damage):
 	health_bar.value -= damage
+	if health_bar.value <= 0:
+		game_over_screen.visible = true
+		get_tree().paused = true
+
+
+
+
+
+func _on_play_again_button_pressed():
+	print("Play again pressed")
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Scenes/anthony_level.tscn")
