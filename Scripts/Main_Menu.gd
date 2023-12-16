206d8244ac32932abf2ff0407adeaa52d6fe90de
Hurkaleez <matthew.morgan105@gmail.com>
2023-12-16 15:35:21 -0500

    added menu background

--- Scripts/Main_Menu.gd
+++ Scripts/Main_Menu.gd
@@ -9,3 +9,11 @@ func _on_quit_button_pressed():
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/anthony_level.tscn")
+
+
+func _on_texture_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/anthony_level.tscn")
+
+
+func _on_texture_button_2_pressed():
+	get_tree().quit()
